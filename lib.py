ba6912f60ddd22d58cc50e819af64e71e9e799c2
Ming Chen <ming.chen@triflesoft.org>
2023-07-17 22:44:31 +0400

    pytest_passed_black_passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 1687916896194439306
+# 28284168961947117523
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d <= 0:
+        if d < 0:
             return []
 
         if d == 0:
--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,4 @@
-# 11313168961944311695
+# 17724168961947116718
 from lib import QuadraticEquationSolver
 from pytest import approx, raises
 
